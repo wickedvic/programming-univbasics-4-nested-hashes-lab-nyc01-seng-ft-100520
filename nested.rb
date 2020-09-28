3acb12a770928126987d872d1e4232a40697bd58
Vivek Sanghvi <viksipart@gmail.com>
2020-09-28 20:13:44 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -104,7 +104,7 @@ def changing_alan
         :languages => ["C"]
       }
     }
-programmer_hash[:alan_kay][:known_for] = alans_new_info
+programmer_hash[:alan_kay][:known_for] = "GUI"
 	programmer_hash
 end
 
